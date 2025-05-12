c98a14a269025d53a890dc9bfcd9fa82b494c353
tomerbuber <tombub@gmail.com>
2025-05-12 14:59:23 +0300

    Added tests

---
+++ test4-1_thread_4_process.cpp
@@ -0,0 +1,168 @@
+/**
+ * @brief run 1 thread on 4 diffrent data 
+ * check for thread safe
+ */
+#include <iostream>
+#include <unistd.h>
+#include "MapReduceClient.h"
+#include "MapReduceFramework.h"
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include  <sys/types.h>
+#include  <stdio.h>
+#include <sys/wait.h>
+#include <set>
+
+
+unsigned int unique_keys = 100;
+
+class elements : public K1,public K2,public K3,public V1,public V2,public V3
+{
+public:
+
+    elements(int i){num = i;}
+
+    bool operator<(const K1 &other) const
+    {
+        return num < dynamic_cast< const elements &>(other).num;
+    }
+
+    bool operator<(const K2 &other) const
+    {
+        return num < dynamic_cast< const elements &>(other).num;
+    }
+
+    bool operator<(const K3 &other) const
+    {
+        return num < dynamic_cast< const elements &>(other).num;
+    }
+
+    bool operator<(const V1 &other) const
+    {
+        return num < dynamic_cast< const elements &>(other).num;
+    }
+
+    bool operator<(const V2 &other) const
+    {
+        return num < dynamic_cast< const elements &>(other).num;
+    }
+
+    bool operator<(const V3 &other) const
+    {
+        return num < dynamic_cast< const elements &>(other).num;
+    }
+
+    int num;
+};
+
+class tester : public MapReduceClient
+{
+public:
+
+    virtual void map(const K1* key, const V1* val, void* context) const
+    {
+        int input = (((elements*)key)->num) % unique_keys;
+        emit2(new elements(input),new elements(1), context);
+    }
+
+    virtual void reduce(const IntermediateVec* pairs, void* context) const
+    {
+        emit3(new elements(((elements*)pairs->at(0).first)->num),new elements((int)pairs->size()), context);
+        for (const IntermediatePair& pair: *pairs) {
+            delete pair.first;
+            delete pair.second;
+        }
+    }
+};
+
+
+struct ThreadSafe {
+    unsigned int threadsNumber;
+    InputVec inputVec;
+    OutputVec outputVec;
+};
+
+/**
+ * @brief The main function running the program.
+ */
+int main(int argc, char *argv[])
+{
+    unsigned int numOfProccess = 4;
+    unsigned int numOfThreads = 1;
+    int r;
+    int modulu_val;
+    std::vector<ThreadSafe> contexts;
+    std::vector<pthread_t> threads;
+    std::vector<InputVec> in_items_vec;
+    std::vector<OutputVec> out_items_vec;
+    threads.resize(numOfProccess);
+    in_items_vec.resize(numOfProccess);
+    out_items_vec.resize(numOfProccess);
+    contexts.resize(numOfProccess);
+
+    std::vector<JobHandle > jobs;
+    tester client;
+    jobs.resize(numOfProccess);
+
+
+
+    for (unsigned int l = 0; l < numOfProccess; ++l)
+    {
+        std::vector<int> numbers(unique_keys, 0);
+        std::srand(l);
+
+        for (int j = 0; j < 100000; ++j)
+        {
+            r = std::rand();
+            modulu_val = r % unique_keys;
+            numbers[modulu_val]++;
+            in_items_vec[l].push_back(std::pair<K1 *, V1 *>(new elements(r),
+                                                          NULL));
+        }
+        contexts[l] = {numOfThreads, in_items_vec[l], out_items_vec[l]};
+    }
+    for (int i = 0; i < numOfProccess; ++i) {
+        jobs[i] = startMapReduceJob(client,contexts[i].inputVec,contexts[i].outputVec,contexts[i].threadsNumber);
+    }
+    for (int i = 0; i < numOfProccess; ++i) {
+  
+      //waitForJob (jobs[i]);
+        closeJobHandle(jobs[i]);
+
+    }
+
+
+    struct {
+        bool operator()(OutputPair& a, OutputPair& b) const
+        {
+            return *a.first < *b.first;
+        }
+    } customLess;
+    for (int m = 0; m < numOfProccess; ++m)
+    {
+        std::sort((contexts[m].outputVec).begin(), (contexts[m].outputVec).end(), customLess);
+
+        for (int j=0; j < (contexts[m].outputVec).size(); j++)
+        {
+            int value = ((elements *)(contexts[m].outputVec)[j].second)->num;
+            std::cout<<"thread "<<m+1<<" out is:\t"<< value <<std::endl;
+        }
+
+        for (int k = 0; k < (contexts[m].outputVec).size(); k++)
+        {
+            delete (contexts[m].outputVec)[k].first;
+            delete (contexts[m].outputVec)[k].second;
+        }
+
+
+        for (int k = 0; k < (contexts[m].inputVec).size(); k++)
+        {
+            delete (contexts[m].inputVec)[k].first;
+            delete (contexts[m].inputVec)[k].second;
+        }
+
+    }
+
+    return 0;
+}
